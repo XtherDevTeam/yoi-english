1b242eb8dca9185ccded69f4f929ea13736a4918
xiaokang00010 <zhouyongkang4321@gmail.com>
2025-02-15 18:08:16 +0800

    feat: improved the view of user banner

--- client/components/UserBanner.js
+++ client/components/UserBanner.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
 
-import { Platform } from 'react-native';
+import { Platform, ScrollView } from 'react-native';
 import {
   Appbar,
   PaperProvider,
   Card,
   Text,
   Avatar,
-  Chip
+  Chip,
+  Dialog,
+  Button,
+  Portal
 } from 'react-native-paper';
 import * as Remote from '../shared/remote'
 import { useFocusEffect } from '@react-navigation/native';
@@ -21,6 +24,7 @@ export default function UserBanner({ userId, style, children, onRecv, reloadTrig
     }
   });
   const [avatarUrl, setAvatarUrl] = React.useState(null);
+  const [infoDialogOpen, infoDialogSetOpen] = React.useState(false);
 
   React.useEffect(() => {
     console.log('triggered reload', userId)
@@ -33,7 +37,7 @@ export default function UserBanner({ userId, style, children, onRecv, reloadTrig
         } else {
           console.log('failed to get user info', data);
         }
-      }); 
+      });
     } else {
       Remote.myInfo().then(data => {
         if (data.status) {
@@ -54,17 +58,34 @@ export default function UserBanner({ userId, style, children, onRecv, reloadTrig
 
   return <>
     <Card style={{ ...style }}>
+      <Portal>
+      <Dialog visible={infoDialogOpen} onDismiss={() => infoDialogSetOpen(false)}>
+        <Dialog.Title>用户近期训练情况</Dialog.Title>
+        <Dialog.Content>
+          <ScrollView style={{ maxHeight: 300 }}>
+          <Text>{userDetails?.overallPerformance}</Text>
+          </ScrollView>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={() => infoDialogSetOpen(false)}>
+            关闭
+          </Button>
+        </Dialog.Actions>
+      </Dialog>
+      </Portal>
       <Card.Cover source={require('../assets/headimg.jpg')} style={{ height: 150 }} />
       <Card.Title title={<Text variant="titleLarge">{userDetails?.username}</Text>} subtitle={userDetails?.email} left={(props) => <Avatar.Image size={50} source={{ uri: avatarUrl }} />} />
       <Card.Content>
         <Chip icon="information" >
           总体评分: {userDetails?.overallBand}
         </Chip>
-        <Text variant="bodyMedium" style={{ marginTop: 10 }}>
-          {userDetails?.overallPerformance || '暂无数据。通过参与写作、学术阅读、口语测试，用户可以获得由AI生成的近期训练情况反馈，以帮助用户更好地提高英语成绩。'}
+        <Text variant="bodyMedium" style={{ marginTop: 10 }} onPress={() => {
+          infoDialogSetOpen(true)
+        }}>
+          {`${userDetails?.overallPerformance?.substring(0, 100)}${userDetails?.overallPerformance?.length > 100 ? "..." : ""}` || '暂无数据。通过参与写作、学术阅读、口语测试，用户可以获得由AI生成的近期训练情况反馈，以帮助用户更好地提高英语成绩。'}
         </Text>
         {children}
       </Card.Content>
-    </Card>
+    </Card >
   </>
 }
